72563fd7b51eff6bb61305aacea1690040ad8d89
Krishna <106014328+krishnask18@users.noreply.github.com>
2024-01-13 15:57:32 +0530

    card class

---
+++ WaveSim/src/card.js
@@ -0,0 +1,13 @@
+class Card {
+    constructor(color="#4ba3b1", 
+                title="title", 
+                desc="description", 
+                img="path", 
+                onclick="url"){
+        this.color = color;
+        this.title = title;
+        this.desc = desc;
+        this.img = img;
+        this.onclick = onclick;
+    }
+}
