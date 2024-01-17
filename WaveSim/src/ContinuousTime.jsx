41dbdbb584bec0aec62dc617a81f457b4ab93bf5
chandan-yadav0011 <chandanyadav8145@gmail>
2024-01-17 14:52:50 +0530

    continuoustimeUpdate

--- WaveSim/src/ContinuousTime.jsx
+++ WaveSim/src/ContinuousTime.jsx
@@ -3,10 +3,87 @@ import Cardcomp from "./Cardcomp";
 function ContinuousTime()
 {
 
-    return <div>
-        <Cardcomp
-          bgim="linear-gradient(to bottom,rgba(79, 112, 65, 1), rgba(63, 87, 54, 0.5)),url(../src/assets/CT_bgi.jpg)"
-          title="Continuous Time Signals"/>
+    return  <div
+    style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" ,transform:"translate(0px, 80px)"}}
+  >
+        <div className="card-hover" 
+          onClick={()=>{
+            window.location="";
+                }}>
+          <Cardcomp
+            bgim="linear-gradient(to bottom,rgba(76, 105, 152, 1), rgba(21, 47, 110)),url(../src/assets/DT_bgi.jpg)"
+            title="Digital Signal Processing"
+            description={
+              <div style={{width:"280px"}}>
+                <div>
+                  {"\u25CF"} Interepretetion & visualisation of DTFT, DFT, {"\u0396"} Transform for discrete time & digital signals
+                </div>
+                <div style={{transform:"translate(0px, 15px)"}}>
+                  {"\u25CF"} Various types of convolutions & animations for Discrete time signals
+                </div>
+              </div>
+            }
+            />
+        </div>
+
+        <div className="card-hover" 
+          onClick={()=>{
+            window.location="/continuousTime";
+                }}>
+          <Cardcomp
+            bgim="linear-gradient(to bottom,rgba(122, 106, 85, 1), rgba(92, 68, 55, 0.5)),url(../src/assets/DT_bgi.jpg)"
+            title="Digital Signal Processing"
+            description={
+              <div style={{width:"280px"}}>
+                <div>
+                  {"\u25CF"} Interepretetion & visualisation of DTFT, DFT, {"\u0396"} Transform for discrete time & digital signals
+                </div>
+                <div style={{transform:"translate(0px, 15px)"}}>
+                  {"\u25CF"} Various types of convolutions & animations for Discrete time signals
+                </div>
+              </div>
+            }
+            />
+        </div>
+        <div className="card-hover" 
+          onClick={()=>{
+            window.location="/continuousTime";
+                }}>
+          <Cardcomp
+            bgim="linear-gradient(to bottom,rgba(15, 126, 94, 1), rgba(10, 115, 86, 0.5)),url(../src/assets/DT_bgi.jpg)"
+            title="Digital Signal Processing"
+            description={
+              <div style={{width:"280px"}}>
+                <div>
+                  {"\u25CF"} Interepretetion & visualisation of DTFT, DFT, {"\u0396"} Transform for discrete time & digital signals
+                </div>
+                <div style={{transform:"translate(0px, 15px)"}}>
+                  {"\u25CF"} Various types of convolutions & animations for Discrete time signals
+                </div>
+              </div>
+            }
+            />
+        </div>
+        <div className="card-hover" 
+          onClick={()=>{
+            window.location="/continuousTime";
+                }}>
+          <Cardcomp
+            bgim="linear-gradient(to bottom,rgba(15, 126, 94, 1), rgba(10, 115, 86, 0.5)),url(../src/assets/DT_bgi.jpg)"
+            title="Digital Signal Processing"
+            description={
+              <div style={{width:"280px"}}>
+                <div>
+                  {"\u25CF"} Interepretetion & visualisation of DTFT, DFT, {"\u0396"} Transform for discrete time & digital signals
+                </div>
+                <div style={{transform:"translate(0px, 15px)"}}>
+                  {"\u25CF"} Various types of convolutions & animations for Discrete time signals
+                </div>
+              </div>
+            }
+            />
+        </div>
+
     </div>
 }
 
